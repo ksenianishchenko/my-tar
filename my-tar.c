90a35adc994c0d23c823013c6d0655aac78a7825
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-02-22 14:08:09 -0800

    add more functionality

--- my-tar.c
+++ my-tar.c
@@ -1,28 +1,10 @@
 #include "header.h"
 
-void filesFromArchiveToTar(char *tarFile, t_llist *archive) {
-
-  FILE *tar = fopen( tarFile, "w+" );
-
-  if( !tar ){
-      printf( "%s", "Please specify a tar file name" );
-      return;
-  }
-
-  while (archive != NULL) {
-      addToTar(tar, archive->data);
-      archive = archive->next;
-  }
-
-  fclose(tar);
-
-}
-
-int filesInDir(char *path, int argc, char **argv, t_llist *filesList) {
+int filesInDir(char *path, int argc, char **argv, char *tarFile) {
     DIR *folder = opendir(path);
     struct dirent *entry = NULL;
 
-    FILE *tar = fopen( argv[2], "w+" );
+    FILE *tar = fopen( tarFile, "w+" );
 
     if( !tar ){
         printf( "%s", "Please specify a tar file name" );
@@ -40,17 +22,15 @@ int filesInDir(char *path, int argc, char **argv, t_llist *filesList) {
 
     while((entry=readdir(folder))) {
 
-      for (int i = 3; i < argc; i++) {
+      for (int i = 1; i < argc; i++) {
 
-        if (strcmp(entry->d_name, argv[i]) == 0) {
-          addToArchive(&filesList, entry->d_name, sizeof(struct dirent));
+        if (strcmp(entry->d_name, argv[i]) == 0 && strcmp(entry->d_name, tarFile) != 0) {
+          addToTar(tar, entry->d_name);
         }
 
       }
 
     }
-
-    filesFromArchiveToTar(argv[2], filesList);
 
     fclose(tar);
 
@@ -59,28 +39,76 @@ int filesInDir(char *path, int argc, char **argv, t_llist *filesList) {
     return 0;
 };
 
+char *getOptions(int argc, char **argv) {
+  char *optionsStr;
+  int argvLen = 0;
+  int n = 0;
+  int j = 0;
+
+    for (int i = 1; i < argc; ++i) {
+        argvLen = argvLen + strlen(argv[i]);
+    };
+
+  optionsStr = malloc(sizeof(char) * argvLen + 1);
+
+  for (int i = 0; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      int tempLeng = strlen(argv[i]);
+      for (int j = 0; j < tempLeng; j++) {
+          optionsStr[n] = argv[i][j];
+          n++;
+      }
+    }
+  }
+
+  if(!optionsStr[0]) {
+    printf("%s\n", "Specify options: -c, -f, -t, -x");
+    return NULL;
+  }
+
+  optionsStr[n] = '\0';
+
+  return optionsStr;
+}
+
+char *initTarFile(int argc, char **argv) {
+  char *tarFileName;
+  for (int i = 0; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      int tempLeng = strlen(argv[i]);
+      if(argv[i][tempLeng - 1] == 'f') {
+        tarFileName = (char *)malloc(sizeof(char) * strlen(argv[i+1]) + 1);
+        tarFileName = argv[i + 1];
+      }
+    }
+  }
+
+  return tarFileName;
+}
+
 int main(int argc, char **argv) {
 
-    t_opts options = get_opts(argv[1]);
-    t_llist *filesList = NULL;
+    char *optionsFromCommandLine = getOptions(argc, argv);
+    t_opts options = get_opts(optionsFromCommandLine);
+    char *tar = initTarFile(argc, argv);
 
     checkOptions(&options);
 
     if(options.opts_c) {
 
-      filesInDir(".", argc, argv, filesList);
+      filesInDir(".", argc, argv, tar);
 
     }
 
     if(options.opts_t && options.opts_f) {
 
-        filesNamesInArchive(argv[2]);
+        filesNamesInArchive(tar);
 
     }
 
     if(options.opts_x && options.opts_f) {
 
-        extractFilesFromArchive(argv[2]);
+        extractFilesFromArchive(tar);
 
     }
 
